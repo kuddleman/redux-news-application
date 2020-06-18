ce5268eb31ce854892ee971fea06afde80e7a7d3
Kuddleman <edl1155@gmail.com>
2020-06-18 09:32:44 -0700

    add Header component

---
+++ src/component/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Header } from './Header'
+
+
+//
+
+function App() {
+  return (
+    <div className="App">
+     <p>Hello from App</p>
+     <Header />
+    </div>
+  );
+}
+
+export default App;
